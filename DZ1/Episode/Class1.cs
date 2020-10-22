dac10e73a0b3a94356a9cf6ed74a82ff41ff9469
aname889 <59070361+aname889@users.noreply.github.com>
2020-10-22 11:15:17 +0200

    Add files via upload

---
+++ DZ1/Episode/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EpisodeClass
+{
+    public class Episode
+    {
+        private int NbViewers;
+        private double SumOfScores;
+        private double MaxScore;
+
+        public Episode(int NbViewers, double SumOfScores, double MaxScore)
+        {
+            this.NbViewers = NbViewers;
+            this.SumOfScores = SumOfScores;
+            this.MaxScore = MaxScore;
+        }
+        public Episode()
+        {
+            this.NbViewers = 0;
+            this.SumOfScores = 0;
+            this.MaxScore = 0;
+        }
+
+        public void AddView(double Score)
+        {
+            NbViewers++;
+            SumOfScores += Score;
+            if (Score > MaxScore) MaxScore = Score;
+        }
+
+
+        public double GetMaxScore()
+        {
+            return MaxScore;
+        }
+
+        public int GetViewerCount()
+        {
+            return NbViewers;
+        }
+
+        public double GetAverageScore()
+        {
+            return SumOfScores / NbViewers;
+        }
+    }
+}
