801067c5383f4ddc76a39411a95502686ad9fa4c
aname889 <aname8889@gmail.com>
2020-11-18 00:26:11 +0100

    nova imena klasa

---
+++ DZ2/DZ/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using EpisodeClass;
+using DescriptionClass;
+using System.IO;
+using TvUtilitiesClass;
+
+namespace DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Description description = new Description(1, TimeSpan.FromMinutes(45), "Pilot");
+            Console.WriteLine(description);
+            Episode episode = new Episode(10, 88.64, 9.78, description);
+            Console.WriteLine(episode);
+
+            string fileName = "shows.txt";
+            //string fileName = "shows.tv";
+            string[] episodesInputs = File.ReadAllLines(fileName);
+            Episode[] episodes = new Episode[episodesInputs.Length];
+            for (int i = 0; i < episodes.Length; i++)
+            {
+                episodes[i] = TvUtilitiess.Parse(episodesInputs[i]);
+            }
+
+            Console.WriteLine("Episodes:");
+            Console.WriteLine(string.Join<Episode>(Environment.NewLine, episodes));
+            TvUtilitiess.Sort(episodes);
+            Console.WriteLine("Sorted episodes:");
+            string sortedEpisodesOutput = string.Join<Episode>(Environment.NewLine, episodes);
+            Console.WriteLine(sortedEpisodesOutput);
+            File.WriteAllText("sorted.tv", sortedEpisodesOutput);
+        }
+    }
+}
